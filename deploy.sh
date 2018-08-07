f68f0be1af722a5febee59b8b4940248358ba8b3
dylan.yin <dylan.yin@oracle.com>
2018-08-07 22:09:16 +0800

    test

--- deploy.sh
+++ deploy.sh
@@ -16,28 +16,40 @@ msg() {
     printf "\033[1;32m :: %s\n\033[0m" "$1"
 }
 
-if [[ $(git status -s) ]]; then
-    msg "The working directory is dirty, please commit or stash any pending changes"
-    exit 1;
-fi
+#if [[ $(git status -s) ]]; then
+#    msg "The working directory is dirty, please commit or stash any pending changes"
+#    exit 1;
+#fi
 
-msg "/************************/"
-msg "Removing the old website"
-msg "/************************/"
-pushd public
-rm -rf *
-popd
+#msg "/************************/"
+#msg "Removing the old website"
+#msg "/************************/"
+#pushd public
+#rm -rf *
+#popd
 
 msg "/************************/"
 msg "Building the website"
 msg "/************************/"
-hugo
+hugo --quiet --theme=even 
+
+
+msg "/************************/"
+msg "Commit Changes"
+msg "/************************/"
+# Add changes to git.
+pushd public
+git add .
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
 
 msg "/************************/"
 msg "Pushing the updated \`public\` folder to the \`master\` branch"
 msg "/************************/"
-pushd public
-git add *
-git commit -m "$MESSAGE"
-popd
 git push origin master
+popd
